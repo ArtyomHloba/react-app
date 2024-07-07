b28c9bb81aab8ce3411450b3165c4d81b5bc9316
ArtyomHloba <hlobartyom@gmail.com>
2024-07-07 17:15:48 +0300

    Add import image & style module

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,6 +1,9 @@
+import logoImg from "./logo.png"
+import styles from './header.module.css'
+
 function Header(){
-    return <header>
-        Header
+    return <header className={styles.pageHeader}>
+        <img className={styles.logoImg} src={logoImg} alt="logo" />
     </header>
 }
 
