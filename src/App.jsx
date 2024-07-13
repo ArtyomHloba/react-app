a7440c890d84fafef33319e61a4d69b435fa2fac
ArtyomHloba <hlobartyom@gmail.com>
2024-07-13 16:27:02 +0300

    Add navList

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,9 @@
-import { useState } from "react";
-import Counter from "./components/Counter";
-import Header from "./components/Header"
-import Main from "./components/Main";
+import NavLinks from "./components/NavLinks"
 
-function App() {
-  const [isLight, setIsLight] = useState(true)
-
-  const isLogin = true;
-  const user = {
-    specOffers: "You have -20% for all laptops",
-  };
-
-  const buttonStyle ={
-    color: isLight ? "yellow" : "grey"
-  };
-
-  const pageContainerStyle = {
-    minHeight:"100vh",
-    color: isLight ? 'black' : 'white',
-    backgroundColor: isLight ? 'white':"black",
-  }
-
-  function changeTheme() {
-    setIsLight(!isLight)
-  }
-
-  return <>
-      <div style={pageContainerStyle}>
-        <Header isLogin={isLogin}/>
-        <button onClick={changeTheme} style={buttonStyle}>
-          <i className="fa-solid fa-sun"></i>
-        </button>
-        <Main isLogin={isLogin} user={user}/>
-        <Counter />
-      </div>
-  </>
+function App(){
+  return (
+    <NavLinks />
+  )
 }
 
 export default App
