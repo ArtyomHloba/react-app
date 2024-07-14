8bd012e04c2d9d918122bf605c95f3c64ad714b5
ArtyomHloba <hlobartyom@gmail.com>
2024-07-14 16:27:27 +0300

    Add form verstka

--- src/components/LoginForm/index.jsx
+++ src/components/LoginForm/index.jsx
@@ -2,15 +2,54 @@ import { useState } from "react";
 import styles from "./LoginForm.module.css";
 
 function LoginForm() {
-  const [value, setvalue] = useState("name");
+  const [email, setEmeil] = useState("");
+  const [password, setPassword] = useState("");
 
-  const handleInputChange = (e) => {
-    setvalue(e.target.value);
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    setEmeil("");
+    setPassword("");
+  };
+
+  const handleEmailChange = (e) => {
+    setEmeil(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
   };
 
   return (
-    <div>
-      <input type="text" value={value} onChange={handleInputChange} />
+    <div className={styles.formContainer}>
+      <h2 className={styles.formTitle}>LoginForm</h2>
+      <form className={styles.loginForm} onSubmit={handleFormSubmit}>
+        <label className={styles.formLabel}>
+          <span className={styles.inputCaption}>Email: </span>
+          <input
+            className={styles.formInput}
+            type="email"
+            name="email"
+            placeholder="yourmail@mail"
+            autoFocus
+            value={email}
+            onChange={handleEmailChange}
+          />
+        </label>
+        <label className={styles.formLabel}>
+          <span className={styles.inputCaption}>Password: </span>
+          <input
+            className={styles.formInput}
+            type="password"
+            name="password"
+            placeholder="enter your password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </label>
+        <button className={styles.submitBtn} type="submit">
+          Login
+        </button>
+      </form>
     </div>
   );
 }
